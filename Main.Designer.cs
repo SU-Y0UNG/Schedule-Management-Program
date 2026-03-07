bba68490af57c84b5a2f4d6ab3bae441d78c15cd
silverrain <rubin053029@gmail.com>
2026-03-07 16:33:48 +0900

    수정 테스트

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,18 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(226, 172);
+            button1.Name = "button1";
+            button1.Size = new Size(243, 102);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
             Name = "Main";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
